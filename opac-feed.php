36d48db1de8de0a954273069cab1dbf99b2d2753
Sven-S. Porst <porst@sub.uni-goettingen.de>
2011-04-13 17:13:05 +0200

    fix error in Opac Parameter

--- opac-feed.php
+++ opac-feed.php
@@ -2,7 +2,7 @@
 
 $queryTerm = $_GET['q'];
 $queryTermEscaped = urlencode($queryTerm);
-$baseURL = 'http://opac.sub.uni-goettingen.de/DB=1/XML=1/REL=1/SHRTST=50/';
+$baseURL = 'http://opac.sub.uni-goettingen.de/DB=1/XML=1/REC=1/SHRTST=50/';
 $queryURL = $baseURL . 'CMD?ACT=SRCHA&IKT=1016&SRT=Dya&TRM=' . $queryTermEscaped;
 $opacFeedXML = file_get_contents($queryURL);
 $opacFeed = new DOMDocument();
